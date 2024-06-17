d1b00ddab98785451b9a6cd70a3a516a721af449
Kaan Barmore-Genç <kaan@bgenc.net>
2024-06-17 17:58:06 -0500

    Add tests for gitlab

--- src/lib/integrations/gitlab.ts
+++ src/lib/integrations/gitlab.ts
@@ -6,7 +6,6 @@ import {
   EIntegrationTargetError,
 } from "./error";
 import { EErrorOptions } from "@lib/utils/error";
-import _ from "lodash";
 
 type EGitLabParseErrorData = EIntegrationParseErrorData & {
   integration: "GitLab";
@@ -21,23 +20,57 @@ export class EGitLabParseError extends EIntegrationParseError {
   }
 }
 
-function isMergeRequestPathname(pathname: string) {
-  return /^[/][^/]+[/][^/]+[/]-[/]merge_requests[/]\d+/i.test(pathname);
+function getCloneDetails({ document, url }: { document: Document; url: URL }) {
+  // The ref selector button is present on the tree view of the repository.
+  const refSelector = document.querySelector(".ref-selector span");
+  // If we are in the tree view, we can get the branch name from the ref
+  // selector, and the repository is always going to match the URL.
+  if (refSelector && refSelector.textContent) {
+    const branch = refSelector.textContent.trim();
+    const repo = /^[/](?<repo>[^/]+[/][^/]+)[/]?/i.exec(url.pathname)?.groups
+      ?.repo;
+    if (repo && branch) {
+      return { repo, branch } as const;
+    }
+  }
+  // This is the element that says "merge [branch] into ...". The href for
+  // the link will always be the repo and the branch that is being merged from,
+  // whether it's a fork or within the same repository.
+  const mrSource = document.querySelector<HTMLAnchorElement>("a.ref-container");
+  if (mrSource && mrSource.href) {
+    const match =
+      /^[/](?<repo>[^/]+[/][^/]+)([/]-[/]tree[/](?<branch>[^?#]+))?/i.exec(
+        mrSource.pathname,
+      )?.groups;
+    if (match && match.repo && match.branch) {
+      return { repo: match.repo, branch: match.branch } as const;
+    }
+  }
+
+  // Otherwise, we don't support wherever we are. Trying to insert links when we
+  // are unsure leads to broken stuff, so we'll just skip.
 }
 
-function parseGitlabPathname(
+function isMergeRequestPathname(pathname: string) {
+  return /^[/][^/]+[/][^/]+[/]-[/]merge_request[/]\d+/i.test(pathname);
+}
+
+function parseGitLabPathname(
   pathname: string,
 ): { repo?: string; branch?: string } | undefined {
-  return /^[/](?<repo>[^/]+[/][^/]+)([/]-[/]tree[/](?<branch>[^/]+))?/i.exec(
+  return /^[/](?<repo>[^/]+[/][^/]+)([/]-[/]tree[/](?<branch>[^?#]+))?/i.exec(
     pathname,
   )?.groups;
 }
 
-
 export const GitLab: Integration = {
   platform: "GitLab",
   supports(url: URL) {
-    return /^https?:[/][/]gitlab.com[/][^/]+[/][^/]+/i.test(url.toString());
+    return (
+      url.hostname === "gitlab.com" &&
+      (isMergeRequestPathname(url.pathname) ||
+        !!parseGitLabPathname(url.pathname))
+    );
   },
   getButtonTarget(document) {
     const node =
@@ -57,13 +90,15 @@ export const GitLab: Integration = {
     return node;
   },
   getCloneUrl({ document, url }) {
-    //
-    if (isMergeRequestPathname(url.pathname)) {
-      url = 
+    const details = getCloneDetails({ document, url });
+    if (details) {
+      const { repo, branch } = details;
+      const branchSuffix = branch ? `@${branch}` : "";
+      return `https://gitlab.com/${repo}${branchSuffix}`;
     }
   },
   buttonClassOverride({ url }) {
-    if (isPR(url)) {
+    if (isMergeRequestPathname(url.pathname)) {
       return "ml-2";
     }
   },
